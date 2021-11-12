725e126453e97ce3ec444faebe16df985ec97de0
Emmanuel Maldonado Cuña <emmanuel.maldonado@kavak.com>
2021-11-12 10:02:59 -0600

    add babel config

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function(api) {
+	api.cache(true);
+	const presets = [
+		[
+			"@babel/preset-env",
+			{
+				targets: {
+					node: "current"
+				}
+			}
+		]
+	];
+	const plugins = [];
+	return {
+		presets,
+		plugins
+	};
+};
