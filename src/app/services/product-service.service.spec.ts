48321661a199802445c03d4e1332505f6b8a2da3
Emmanuel Maldonado Cuña <emmanuel.maldonado@kavak.com>
2021-11-16 17:26:49 -0600

    Service Builder using httpclient as argument

--- src/app/services/product-service.service.spec.ts
+++ src/app/services/product-service.service.spec.ts
@@ -24,22 +24,23 @@ const mockProductsResponse = [
 ];
 
 describe('ProductServiceService', () => {
-  
-  const httpClient: any ={
-    get: jest.fn((url:string)=> ({ pipe: (r, e) => of(mockProductsResponse)}))
-};
 
-  const wrapperService = () => {
+
+  const wrapperService = (httpClient) => {
     return new ProductServiceService(httpClient);
   };
 
   it('should be created', () => {
-    let service = wrapperService();
+    const httpClient: any ={};
+    let service = wrapperService(httpClient);
     expect(service).toBeTruthy();
   });
 
   it('should fetch product list', async () => {
-    let service = wrapperService();
+    const httpClient: any ={
+      get: jest.fn((url:string)=> ({ pipe: (r, e) => of(mockProductsResponse)}))
+  };
+    let service = wrapperService(httpClient);
 
     const products = await lastValueFrom(service.fetchProducts());
     expect(httpClient.get).toHaveBeenCalledWith('http://localhost:3000/products');
